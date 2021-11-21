c32806e3c62504e2930c453f1a7e778923ba3d50
Zhan <67866421+yanxili17@users.noreply.github.com>
2021-11-21 19:25:36 +0900

    Update popup.js
    新增：ctrl键控制开始和结束，进入扩展程序的欢迎语

--- image2U/popup.js
+++ image2U/popup.js
@@ -1,31 +1,53 @@
-window.onload = function(){
-    window.onkeypress=function(e){
-        if(e.charCode == 39){
-            alert('Right click')
-        }
-    }
-}
+//Welcome Message
+var msg1 = new SpeechSynthesisUtterance("Now you can start to convert image to text! Please click or use the ctrl key to start.");
+window.speechSynthesis.speak(msg1);
+
+//mouse event
 document.getElementById("myBtn").onclick = function () { displaytext() };
 
 function displaytext() {
-    if (document.getElementById("myBtn").innerHTML == 'Start') {
-        document.getElementById("myBtn").innerHTML = 'End';
-        var msg = new SpeechSynthesisUtterance("The result is being output. Please click or use keyboard to end");
+    if (document.getElementById("myBtn").innerHTML == 'End') {
+        document.getElementById("myBtn").innerHTML = 'Start';
+        var msg = new SpeechSynthesisUtterance("Please click or use the ctrl key to start.");
         window.speechSynthesis.speak(msg);
-        document.getElementById("result").innerHTML = 'Show results...';
+        document.getElementById("result").innerHTML = '';
     }
     else{
-        document.getElementById("myBtn").innerHTML = 'Start';
-        var msg1 = new SpeechSynthesisUtterance("Please click or use keyboard to start");
+        document.getElementById("myBtn").innerHTML = 'End';
+        var msg1 = new SpeechSynthesisUtterance("The result is being output. Please click or use the ctrl key to end.");
+        var msg_result = new SpeechSynthesisUtterance("Read the result.");
         window.speechSynthesis.speak(msg1);
-        document.getElementById("result").innerHTML = '';
+
+        window.speechSynthesis.speak(msg_result);
+        document.getElementById("result").innerHTML = 'Show results...';
     }
    
 }
-/* Keyboard:
-document.getElementById1("myBtn").onkeypress = function () { displaytext1() };
-function displaytext1(evt) {
-    if (evt.charCode == 70) {
-        document.getElementById1("myBtn").innerHTML = 'click or use keyboard to end';
+
+//keyboard event
+document.addEventListener("keydown", keydown);
+//Parameter 1: Indicates the event, keydown: Press down on the keyboard; 
+//Parameter 2: Indicates the event to be triggered
+
+
+function keydown(event) {
+    switch (event.keyCode) {
+        case 17:
+            if (document.getElementById("myBtn").innerHTML == 'End') {
+                document.getElementById("myBtn").innerHTML = 'Start';
+                var msg = new SpeechSynthesisUtterance("Please click or use the ctrl key to start.");
+                window.speechSynthesis.speak(msg);
+                document.getElementById("result").innerHTML = '';
+            }
+            else {
+                document.getElementById("myBtn").innerHTML = 'End';
+                var msg1 = new SpeechSynthesisUtterance("The result is being output. Please click or use the ctrl key to end.");
+                var msg_result = new SpeechSynthesisUtterance("Read the result.");
+                window.speechSynthesis.speak(msg1);
+
+                window.speechSynthesis.speak(msg_result);
+                document.getElementById("result").innerHTML = 'Show results...';
+            }
+
     }
-}*/
+}
